7fe22d18c5bbaea38a9583e801de2133b9bbc9d1
Nadav Tasger <tashernadav@gmail.com>
2019-07-16 22:24:33 +0300

    Finished

--- app/src/main/java/nadav/tasher/handasaim/MainActivity.java
+++ app/src/main/java/nadav/tasher/handasaim/MainActivity.java
@@ -4,27 +4,57 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.util.Log;
+import android.webkit.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView webView = new WebView(getApplicationContext());
+        // Setup WebView
+        WebView webView = new WebView(this);
+        // Setup cookies
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        // Setup client
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(request.getUrl());
-                getApplicationContext().startActivity(intent);
+                if (!request.getUrl().toString().startsWith(getString(R.string.app_url))) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(request.getUrl());
+                    startActivity(intent);
+                } else {
+                    view.loadUrl(request.getUrl().toString());
+                }
                 return true;
             }
         });
+        // Setup Javascript
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://handasaim.app");
+        // Setup app cache
+        webView.getSettings().setAppCacheEnabled(true);
+        // Load url
+        webView.loadUrl(getString(R.string.app_url));
+        // Set content view
         setContentView(webView);
+        // Cookie flush timer - essential
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                CookieManager.getInstance().flush();
+            }
+        }, 0, 1000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Flush cookies one last time
+        CookieManager.getInstance().flush();
     }
 }
